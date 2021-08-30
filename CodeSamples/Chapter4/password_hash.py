0c6a1c4bd1808edd760058ef98add65b76e7ab64
Will Kahn-Greene <willkg@users.noreply.github.com>
2021-08-30 08:56:52 -0400

    Fix chapter 4 issues (#1)
    * add chapter4 requirements file which covers all chapter4 examples
    * remove unnecessary files
    * add missing template

--- CodeSamples/Chapter4/password_hash.py
+++ CodeSamples/Chapter4/password_hash.py
@@ -1,8 +1,19 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker
+
 from werkzeug.security import generate_password_hash, check_password_hash
+
+
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+engine = create_async_engine(DATABASE_URL, future=True, echo=True)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+Base = declarative_base()
 
 
 class User(Base):
     __tablename__ = "user"
+    id = Column(Integer, primary_key=True, autoincrement=True)
     # ... all the Columns ...
 
     def __init__(self, *args, **kw):
